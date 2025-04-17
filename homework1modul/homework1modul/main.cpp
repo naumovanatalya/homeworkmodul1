7962e03d6b250d8955cb69f9068884767d0b27fa
naumovanatalya <podkina38@gmail.com>
2025-04-17 17:24:14 +0300

    Add files via upload
    Простейший консольный калькулятор, который принимает от пользователя два целых числа и поочередно выводит результаты простейших математических (сложение, вычитание, умножение, деление) и битовых операций (И, ИЛИ, исключающее ИЛИ) для этих чисел.

---
+++ homework1modul/homework1modul/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL,"");
+
+	int a{}, b{};
+	cout << "Введите два числа:" << endl;
+	cin >> a >> b;
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / float(b) << endl;
+	cout << a << " & " << b << " = " << (a & b) << endl;
+	cout << a << " | " << b << " = " << (a | b) << endl;
+	cout << a << " ^ " << b << " = " << (a ^ b) << endl;
+
+}
